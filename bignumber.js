329987935221827642303a7d27d61234ec9e6a0f
Benjamin Meudre <bmeudre@gmail.com>
2014-12-04 19:25:21 -0800

    Version bump

--- bignumber.js
+++ bignumber.js
@@ -1,4 +1,4 @@
-/*! bignumber.js v1.5.0 https://github.com/MikeMcl/bignumber.js/LICENCE */
+/*! bignumber.js v1.5.1 https://github.com/MikeMcl/bignumber.js/LICENCE */
 
 ;(function ( global ) {
     'use strict';
